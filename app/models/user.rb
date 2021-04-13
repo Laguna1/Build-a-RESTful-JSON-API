bf8ece27f376e0630cabc44d84534c07843ccad9
Laguna1 <lagunazoomarket@gmail.com>
2021-04-14 00:52:16 +0300

    Add authentication controller and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_many :todos, foreign_key: :created_by
 
-  validates :name, presence: true, length: { minimum: 3 }
-  validates :email, presence: true, length: { minimum: 3 }
-  validates :password_digest, presence: true, length: { minimum: 6 }
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :todos do
     resources :items
   end
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
